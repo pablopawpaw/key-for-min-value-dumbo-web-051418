0055ca3191085d1bb66bf55fb98552c0ed1b517f
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-05-10 17:59:07 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash.class == Hash
+    puts "Please enter proper hash argument"
+  elsif name_hash.length == 0 
+    nil
+  else 
+    
 end
