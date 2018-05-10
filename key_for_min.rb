c017b81d98fa04e4aec6b7ea8de3c7e413296d98
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-05-10 18:25:55 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,30 +3,26 @@
 
 def key_for_min_value(name_hash)
   #reverse keys and values to a new hash
-new_hash = {}
-name_hash.each do |k,v|
-  new_hash[v] = k
-end 
+  new_hash = {}
+  name_hash.each do |k,v|
+    new_hash[v] = k
+  end 
 
 #loop process to eliminate largest values leaving min behind in hash
-i = 0 
-while i < new_hash.length do 
-largest = new_hash.max
-
-largest = largest.select do |x|
-  x.is_a? Integer
-end 
-
-largest = largest[0]
-
-new_hash.delete(largest)
-i+=1
-end 
+  i = 0 
+  while i < new_hash.length do 
+    largest = new_hash.max
+    largest = largest.select do |x|
+      x.is_a? Integer
+    end 
+    largest = largest[0]
+    new_hash.delete(largest)
+    i+=1
+  end 
 
 #change hash to array for easy access
-min_array = new_hash.to_a.flatten
+  min_array = new_hash.to_a.flatten
 
 #return "key" of min value
-min_array[1]
-    
+  min_array[1]
 end
